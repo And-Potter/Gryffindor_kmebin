aa2d9ac6e70f27677a3f1a631a2f9b501b3ab89d
kmebin <blowhuiee@gmail.com>
2021-04-25 20:39:05 +0900

    [update] homeActivity에 recyclerview 구현

---
+++ app/src/main/java/org/kmebin/seminar28th/ui/HomeActivity.kt
@@ -0,0 +1,48 @@
+package org.kmebin.seminar28th.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.kmebin.seminar28th.adapter.RepositoryListAdapter
+import org.kmebin.seminar28th.data.RepositoryInfo
+import org.kmebin.seminar28th.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var repositoryListAdapter: RepositoryListAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        configureRepositoryList()
+    }
+
+    private fun configureRepositoryList() {
+        repositoryListAdapter = RepositoryListAdapter()
+        binding.repositoryList.adapter = repositoryListAdapter
+
+        repositoryListAdapter.repositoryList.addAll(
+            listOf(
+                RepositoryInfo(
+                    name = "repository1",
+                    description = "description1",
+                    language = "kotlin"
+                ),
+                RepositoryInfo(
+                    name = "repository2",
+                    description = "description2",
+                    language = "Python"
+                ),
+                RepositoryInfo(
+                    name = "repository3",
+                    description = "description3",
+                    language = "JavaScript"
+                ),
+            )
+        )
+        repositoryListAdapter.notifyDataSetChanged()
+    }
+}
